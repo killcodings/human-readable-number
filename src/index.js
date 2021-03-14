cded8f2a3021240a05f48bca0bf49b9e5a9ba68f
killcodings <kontaktid@ya.ru>
2021-03-15 02:27:00 +0300

    docs: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(n) {
+    var str = n.toString();
+
+    if (parseInt(str) === 0) {
+        return "zero";
+    }
+
+    let num1_19 = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let num20_90 = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let numb = "";
+    let c = [];
+    let numbrsName = [];
+    c = str.split("");
+
+    let i = c.reverse().map(parseFloat);
+
+    if (i[1] === 1) {
+        i[0] += 10;
+    }
+    if ((numb = num1_19[i[0]])) {
+        numbrsName.push(numb);
+    }
+    if ((numb = num20_90[i[1]])) {
+        numbrsName.push(numb);
+    }
+    if ((numb = num1_19[i[2]])) {
+        numbrsName.push(numb + " hundred");
+    }
+    return numbrsName.reverse().join(" ");
+};
